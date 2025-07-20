ed86a2f5af046bb8b75b53e8d88f31cacd1f0d3d
testak1 <60112704+testak1@users.noreply.github.com>
2025-07-20 19:56:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -118,4 +118,5 @@ def index():
     return render_template('index.html', results=results)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
